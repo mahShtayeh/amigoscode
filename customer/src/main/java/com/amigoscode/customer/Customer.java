781daa1b139d55455a9075bb616c35f325938a78
Mahmoud Shtayeh <mshtayeh.c@stc.com.sa>
2025-06-09 17:04:30 +0300

    Create Customer Module

---
+++ customer/src/main/java/com/amigoscode/customer/Customer.java
@@ -0,0 +1,35 @@
+package com.amigoscode.customer;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+}
---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package com.amigoscode.customer;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+        // todo: check if email is valid
+        // todo: check if email not taken
+        customerRepository.save(customer);
+    }
+}
