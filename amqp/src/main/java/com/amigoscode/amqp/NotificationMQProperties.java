f059ae3b63ff05928ed518e34ea0e6b8a158a27b
Mahmoud Shtayeh <mshtayeh.c@stc.com.sa>
2025-06-11 18:01:18 +0300

    Centralize RabbitMQ properties

---
+++ amqp/src/main/java/com/amigoscode/amqp/NotificationMQProperties.java
@@ -0,0 +1,20 @@
+package com.amigoscode.amqp;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+@ConfigurationProperties
+public class NotificationMQProperties {
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queues.notification}")
+    private String notificationQueue;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String notificationRoutingKey;
+}
